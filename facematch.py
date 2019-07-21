5156f0d45f1d50b57f636372834332270f3e0069
Veer16 <veer.mailer@gmail.com>
2019-07-21 23:52:00 +1200

    basic testing and experimenting with the fae_recognition package. Project still in process

---
+++ facematch.py
@@ -0,0 +1,19 @@
+import face_recognition
+
+image_of_bill = face_recognition.load_image_file('./img/known/Bill Gates.jpg')
+bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
+
+unkown_image = face_recognition.load_image_file(
+    './img/unknown/bill-gates-4.jpg')
+unkown_face_encoding = face_recognition.face_encodings(unkown_image)[0]
+
+# compare faces
+
+results = face_recognition.compare_faces(
+    [bill_face_encoding], unkown_face_encoding)
+
+if results[0]:
+
+    print('This is Bill Gates')
+else:
+    print('This is NOT Bill Gates')
